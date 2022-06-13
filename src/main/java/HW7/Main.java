62ca7d10ccfee55555a1018a36f3ee8a08824c00
Loi_Emmanuel <emlzoloto@gmail.com>
2022-06-13 12:37:21 +0300

    Added HW7

--- src/main/java/HW7/Main.java
+++ src/main/java/HW7/Main.java
@@ -2,21 +2,21 @@ package HW7;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayString example = new ArrayString();
+        ArrayString sut = new ArrayString();
 
-        example.add("1");
-        example.add("2");
-        example.add("3");
-        example.add("4");
-        example.add("5");
-        example.add("6");
-        example.add(15, "6");
-        example.add(10, "6");
-        System.out.println(example);
-        System.out.println(example.get(5));
-        example.delete(5);
-        System.out.println(example);
-        System.out.println(example.delete("2"));
-        System.out.println(example);
+        sut.add("1");
+        sut.add("2");
+        sut.add("3");
+        sut.add("4");
+        sut.add("5");
+        sut.add("6");
+        sut.add(15, "6");
+        sut.add(10, "6");
+        System.out.println(sut);
+        System.out.println(sut.get(5));
+        sut.delete(5);
+        System.out.println(sut);
+        System.out.println(sut.delete("2"));
+        System.out.println(sut);
     }
 }
